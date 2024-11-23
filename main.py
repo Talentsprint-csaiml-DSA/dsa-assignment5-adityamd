1d705baefcb3eb94717449595010d5a9dfc02cfe
Aditya Tripathi <aditya.t2210@gmail.com>
2024-11-24 01:15:39 +0530

    Solution

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+def soln(coins, target_amount, sm):
+    if sm == target_amount:
+        return 0
+    mn = -1
+    for i in coins:
+        if sm + i <= target_amount:
+            tmp = 1 + soln(coins, target_amount, sm + i)
+            if tmp == 0:
+                return -1
+            if mn == -1 or mn > tmp:
+                mn=tmp
+    return mn
+ 
 def min_coins(coins, target_amount):
-    ""code here""
+    return soln(coins, target_amount, 0)
